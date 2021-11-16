c94fc901d92f057a67beeaca4a672f94e81fa6c4
Dao Quang Phuong <daoquangphuong@gmail.com>
2021-11-17 06:10:11 +0700

    update force update

--- src/index.js
+++ src/index.js
@@ -8,24 +8,21 @@ const { Worker } = require('worker_threads');
 const path = require('path');
 const update = require('./update');
 
-update
-  .check()
-  .then(() => {
-    const startServer = () => {
-      const worker = new Worker(path.resolve(__dirname, 'server.js'));
-      worker.on('message', data => {
-        if (data && data.event === 'update') {
-          worker.terminate();
-          startServer();
-        }
-      });
-      worker.on('error', console.error);
-      worker.on('exit', code => {
-        if (code !== 0) {
-          console.error(`Worker stopped with exit code ${code}`);
-        }
-      });
-    };
-    startServer();
-  })
-  .catch(console.error);
+const main = async () => {
+  await update.check();
+  const worker = new Worker(path.resolve(__dirname, 'server.js'));
+  worker.on('message', data => {
+    if (data && data.event === 'update') {
+      worker.terminate();
+      main().catch(console.error);
+    }
+  });
+  worker.on('error', console.error);
+  worker.on('exit', code => {
+    if (code !== 0) {
+      console.error(`Worker stopped with exit code ${code}`);
+    }
+  });
+};
+
+main().catch(console.error);
