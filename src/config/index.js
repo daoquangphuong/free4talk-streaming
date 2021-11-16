01509007bb44fc8bff37488670708ff13e40790f
Dao Quang Phuong <daoquangphuong@gmail.com>
2021-11-17 04:27:27 +0700

    move tmp folder

--- src/config/index.js
+++ src/config/index.js
@@ -7,15 +7,15 @@ module.exports = {
     uploads: 0, // Number of upload slots.
     tmp: path.resolve(
       __dirname,
-      process.env.NODE_ENV !== 'production' ? '../../dest/tmp' : 'tmp'
+      process.env.NODE_ENV !== 'production' ? '../../tmp' : '../tmp'
     ), // Root folder for the files storage.
     // Defaults to '/tmp' or temp folder specific to your OS.
     // Each torrent will be placed into a separate folder under /tmp/torrent-stream/{infoHash}
     path: path.resolve(
       __dirname,
       process.env.NODE_ENV !== 'production'
-        ? '../../dest/tmp/my-files'
-        : 'tmp/my-files'
+        ? '../../tmp/my-files'
+        : '../tmp/my-files'
     ), // Where to save the files. Overrides `tmp`.
     verify: true, // Verify previously stored data before starting
     // Defaults to true
