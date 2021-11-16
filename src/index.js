2cf5d51c886a4c9d3d7ee16767074a2450ea76aa
Dao Quang Phuong <daoquangphuong@gmail.com>
2021-11-17 04:32:10 +0700

    fix bugs

--- src/index.js
+++ src/index.js
@@ -14,15 +14,16 @@ const app = express();
 app.use('/streaming', cors(), routes);
 
 app.listen(config.port, () => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.info(
+      `You can now view free4talk-streaming-server in the browser. \n\n http://localhost:${config.port} \n`
+    );
+    return;
+  }
   console.info(
     `
 Free4Talk-Streaming-Server is started.
 You can comeback Free4Talk and start to use Streaming.
   `.trim()
   );
-  if (process.env.NODE_ENV !== 'production') {
-    console.info(
-      `You can now view free4talk-streaming-server in the browser. \n\n http://localhost:${config.port} \n`
-    );
-  }
 });
